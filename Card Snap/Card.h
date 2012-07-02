0d0711bd23ccb127f4056c026fa0a4c2e6c342fb
Jacob Groundwater <groundwater@gmail.com>
2012-07-02 16:32:39 -0700

    Add XCode Generated Card View

---
+++ Card Snap/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Card Snap
+//
+//  Created by jacob on 12-07-02.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Card : UIViewController
+
+@end
