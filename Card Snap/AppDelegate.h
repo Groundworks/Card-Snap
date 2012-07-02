07cf2f5f4b85ee10d6b04adae97820694e40a647
Jacob Groundwater <groundwater@gmail.com>
2012-07-02 16:22:39 -0700

    Initial Commit

---
+++ Card Snap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Card Snap
+//
+//  Created by jacob on 12-07-02.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
