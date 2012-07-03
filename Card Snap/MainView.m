042255904f8f97c115ffb01eda8424e3288c8f81
Jacob Groundwater <groundwater@gmail.com>
2012-07-02 22:32:19 -0700

    Adjust Spacing

--- Card Snap/MainView.m
+++ Card Snap/MainView.m
@@ -21,9 +21,10 @@
 
 - (void)viewWillAppear:(BOOL)animated
 {
-    CGFloat  width = self.view.bounds.size.width;
+    CGFloat width = self.view.bounds.size.width;
     
     cards = [[NSMutableArray alloc]init];
+    
     for(int i=0; i<10; i++)
     {
         Card *cardView = [[Card alloc]initWithNibName:nil bundle:nil];
@@ -32,12 +33,12 @@
         cardWidth  = cardView.view.bounds.size.width;
         
         [cardView.view setFrame:CGRectMake(0.0, 
-                                       self.view.bounds.size.height/2.0+
-                                       cardHeight/2.0, 
-                                       cardWidth,
-                                       cardHeight)];
-        [cards addObject:cardView];
-        [scrollView addSubview:cardView.view];
+                self.view.bounds.size.height/2.0+
+                cardHeight/2.0, 
+                cardWidth,
+                cardHeight)];
+        [cards      addObject  :cardView];
+        [scrollView addSubview :cardView.view];
 
     }
     
@@ -49,27 +50,27 @@
 {
     // The offset moves in response to a drag or flick gesture
     CGPoint offset = [sv contentOffset];
+    CGFloat yoffset = offset.y;
     
     CGFloat width  = sv.bounds.size.width;
     CGFloat height = sv.bounds.size.height;
-    
-    CGFloat yoffset = offset.y;
     
     int ncards = [cards count];
     for(int i=0; i<ncards; i++)
     {   
         
-        UIViewController  *cardView  = [cards objectAtIndex:i];
+        UIViewController
+            *cardView  = [cards objectAtIndex:i];
         UIView  *view  = cardView.view;
         CALayer *layer = view.layer;
         
-        CGFloat diff = (view.center.y - yoffset - height/2.0)/height;
+        CGFloat diff  = (view.center.y - yoffset - height/2.0) / height;
         CGFloat scale = expf(-diff*diff);
                 
         // Core Animation - Affine Transformation
         CATransform3D resize = CATransform3DMakeScale(scale, scale, scale);
         
-        [view setCenter:CGPointMake(width/2.0, (cardHeight)*(i+1))];
+        [view  setCenter:CGPointMake(width/2.0, (cardHeight)*(i+1))];
         [layer setTransform:resize];
     }
 
